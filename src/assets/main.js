b34f5b10a58b7b47a166e11e55ed9bac91a61bc8
wpelczar <wpelczar1@gmail.com>
2016-10-30 22:58:47 +0100

    display badges

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,18 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/2094378.json',
+    dataType: 'jsonp',
+    success: function(response){
+      $.each(response.courses.completed, function(index, value){
+        var course = $('<div class="course"></div>');
+        course.append($('<h3>', { text: value.title }));
+        course.append($('<img>', { src: value.badge }));
+        course.append($('<a>', { href: value.url, target: '_blank', class: 'btn btn-primary', text: 'See course' }));
+        $('#badges').append(course); 
+      });
+    }
+  })
 
 });
